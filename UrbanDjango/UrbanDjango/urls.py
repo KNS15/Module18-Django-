35f8e0bcac4dfbe6600e9f95c6f8ebdd256d7366
KNS15 <167962653+KNS15@users.noreply.github.com>
2024-11-03 17:29:35 +0400

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for UrbanDjango project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
@@ -15,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('task2.urls', namespace='task2')),
+    path('', include('task4.urls', namespace='task4')),
+    path('', include('task5.urls', namespace='task5'))
 ]
